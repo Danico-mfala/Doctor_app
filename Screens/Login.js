c4aa69de7b84b8cad12b0f38e37183df2f86d9d9
Danico-Mfala <mfaladan@gmail.com>
2024-02-01 22:43:02 -0800

    Designing Home page

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,25 +1,42 @@
 import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
 import imageLogin from "./../assets/Images/image_1.jpg";
+import Colors from "../assets/Shared/Colors";
 
 export default function Login() {
   return (
-    <View>
+    <View style={styles.container}>
       <Image source={imageLogin} style={styles.LoginImage} />
 
-      <View style={{ backgroundColor: "#000" }}>
+      <View style={styles.TextContainer}>
         <Text style={styles.Title}>Welcome to our app!</Text>
-        <Text style={styles.Subtitle}>Please log in.</Text>
+        <Text style={styles.Subtitle}>Appointment Booking App.</Text>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+  },
   LoginImage: {
     width: 410,
     height: 460,
     marginTop: 20,
     objectFit: "contain",
+  },
+  TextContainer: {
+    backgroundColor: Colors.primary,
+    padding: 25,
+    alignItems: "center",
+  },
+  Title: {
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  Subtitle: {
+    fontSize: 30,
+    fontWeight: "500",
   },
 });
---
+++ assets/Shared/Colors.js
@@ -0,0 +1,4 @@
+export default {
+  white: "#fff",
+  primary: "#5DACBD",
+};
